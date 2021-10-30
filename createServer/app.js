7c73c6e9ca229b683db6b20c8928f5add84ac7ec
Robin <robinburri@tutanota.com>
2021-10-30 20:15:00 +0200

    add methode

--- createServer/app.js
+++ createServer/app.js
@@ -1,8 +1,27 @@
 const http = require("http");
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    // process.exit();
+    const url = req.url;
+    const methode = req.method;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" methode="POST"><input type="text" name="message"></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && methode === 'POST') {
+            fs.writeFileSync('message.txt', 'DUMMY');
+            res.statusCode  = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My first Page</title></head>');
+    res.write('<body><h1>Hello from my node.js</h1></body>');
+    res.write('</html>');
 });
 
 server.listen(3000);
